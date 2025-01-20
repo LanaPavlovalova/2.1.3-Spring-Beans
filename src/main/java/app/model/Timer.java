e7bdda3d03e387e99f5ca78c7f4cee1bbe6aa144
Lana Pavlova <lana_eva@list.ru>
2025-01-20 13:08:41 +0500

    Добавлен проект и создана и добавлена папка Дог

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -4,6 +4,14 @@ public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
+    public Timer(Long nanoTime) {
+        this.nanoTime = nanoTime;
+    }
+
+    public Timer() {
+
+    }
+
     public Long getTime() {
         return nanoTime;
     }
